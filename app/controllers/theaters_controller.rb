65fc2c63bfd29ef0d5966ac063a736c7f8e96ff8
Eriche Gonzales <erichelouise@gmail.com>
2022-08-30 16:53:09 -0400

    viewing individual theaters in database

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,7 +1,15 @@
 class TheatersController < ApplicationController
+    # GET /
     def index
         theaters = Theater.all
         render json: theaters.to_json(methods: [:movies])
     end
 
+    # GET /theaters/:id
+    def show
+        theater = Theater.find_by!(id: params[:id])
+        render json: theater.to_json(methods: [:movies])
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "theaters#index"
+  get "/theaters/:id", to: "theaters#show"
 end
